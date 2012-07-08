aa65ae62533764d67d216baae321bc4441d40c5f
syamgot <syamgot@gmail.com>
2012-07-09 05:34:31 +0900

    フォルダ構成変更

---
+++ tests/AllTests.php
@@ -0,0 +1,33 @@
+<?php
+//require_once 'PHPUnit/Framework/TestSuite.php';
+
+class AllTests {
+	
+	public static function suite() {
+		
+		$suite = new PHPUnit_Framework_TestSuite();
+
+		foreach (self::$tests as $value)
+		{
+			require_once $value.'.php';
+			$suite->addTestSuite($value);
+		}
+		
+		return $suite;
+	}
+
+	protected static $tests = array(
+		  'ValidatorTest'
+		, 'AlumValidatorTest'
+		, 'DataTypeValidatorTest'
+		, 'GEValidatorTest'
+		, 'GTValidatorTest'
+		, 'LEValidatorTest'
+		, 'LTValidatorTest'
+		, 'NotEmptyValidatorTest'
+		, 'NotNullValidatorTest'
+		, 'RegularExpressionValidatorTest'
+		, 'StrLengthValidatorTest'
+		, 'ValidatorTemplatesTest'
+	);
+}
---
+++ tests/LtValidatorTest.php
@@ -0,0 +1,111 @@
+<?php
+
+require_once __DIR__ . '/../validator/LTValidator.php';
+
+/**
+ * LTValidator unit test
+ * 
+ * @author syamgot
+ */
+class LTValidatorTest extends PHPUnit_Framework_TestCase {
+	
+	/** **************************************************
+	*
+	* Tests
+	*
+	************************************************** */
+    
+	/**
+	 * @dataProvider providerIsValid
+	 */
+	public function testIsValid($Max, $val, $res) {
+		self::$obj->setMax($Max);
+		$this->assertEquals(self::$obj->isValid($val), $res);
+	}
+	
+	/**
+	 * @dataProvider providerConstruct
+	 */
+	public function testConstruct($param, $val, $res) {
+		$v = new LTValidator($param);
+		$this->assertEquals($v->isValid($val), $res);
+	}
+	
+
+	/** **************************************************
+	*
+	* setup and teardown
+	*
+	************************************************** */
+	
+	/**
+	 * 
+	 */	
+	public static function setUpBeforeClass() {
+		self::$obj = new LTValidator();
+	}
+	
+	/**
+	 * 
+	 */
+	protected function setUp() {}
+	
+	/**
+	 * 
+	 */
+	protected function tearDown() {}
+	
+	/**
+	 * 
+	 */
+	public static function tearDownAfterClass() {}
+	
+	
+	/** **************************************************
+	*
+	* Static Methods
+	*
+	************************************************** */
+	
+	/**
+	 * @var LTValidator
+	 */
+	protected static $obj;
+	
+	
+	/** **************************************************
+	 * 
+	 * Data Providers
+	 * 
+	 ************************************************** */
+	
+    /**
+     * 
+     */
+    public function providerIsValid() {
+    	return array(
+    		  array(5, 4, true)
+    		, array(5, 5, false)
+    		, array(5, 6, false)
+    		, array(0, -1, true)
+    		, array(0, 0, false)
+    	);
+    }
+    
+    /**
+     * 
+     */
+    public function providerConstruct() {
+    	return array(
+	    	  array(5, 4, true)
+	    	, array(5, 5, false)
+	    	, array(5, 6, false)
+	    	, array(array('max'=>5), 4, true)
+	    	, array(array('max'=>5), 5, false)
+	    	, array(array('max'=>5), 6, false)
+    	);
+    }
+
+
+}
+
